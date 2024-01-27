bf1aadfee937962a0390cb30bf7398799aa66e1b
Christian Ochoa <a20110469@live.ceti.mx>
2024-01-27 00:03:16 -0600

    Server route to update products

--- server/api/database/query/edit.patch.ts
+++ server/api/database/query/edit.patch.ts
@@ -7,16 +7,23 @@ export default defineEventHandler(async(event) => {
     const body: EditItem = await readBody(event)
     try {
         // search item
-        const { rows } = await sql`SELECT id FROM Items Where id = ${body.id}`
+        const { rows } = await sql`SELECT name, description, price FROM Items Where id = ${body.id}`
         if (rows.length === 0) {
             setResponseStatus(event, 404)
             return {
                 message: 'Item not found'
             }
         }
+        const name = body.name ? body.name : rows[0].name
+        const description = body.description ? body.description : rows[0].description
+        const price = body.price ? body.price : rows[0].price
         // item exist
-        await sql`UPDATE Items SET name = ${body.name}, description = ${body.description}, price = ${body.price}, updated_at = NOW() 
+        await sql`UPDATE Items SET name = ${name}, description = ${description}, price = ${price}, updated_at = NOW() 
                   WHERE id = ${body.id}`
+        setResponseStatus(event, 201)
+        return {
+            message: 'Item updated'
+        }
     } catch (error) {
         setResponseStatus(event, 500)
         return { 
@@ -24,5 +31,4 @@ export default defineEventHandler(async(event) => {
             message: 'Error while searching item'
         }
     }
-    return { body }
 })
